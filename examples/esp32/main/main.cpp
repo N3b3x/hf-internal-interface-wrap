74911a7beb19878467b5437c3ce99e0b5341223c
Nebiyu Tadesse <nebysma@gmail.com>
2025-06-15 21:54:47 -0600

    Format ESP32 example

--- examples/esp32/main/main.cpp
+++ examples/esp32/main/main.cpp
@@ -1,15 +1,15 @@
 #include "DigitalOutput.h"
-#include "I2cBus.h"
 #include "DummyDevice.hpp"
+#include "I2cBus.h"
 
 extern "C" void app_main(void) {
-    i2c_config_t cfg{};
-    cfg.sda_io_num = GPIO_NUM_21;
-    cfg.scl_io_num = GPIO_NUM_22;
-    I2cBus bus(I2C_NUM_0, cfg);
-    bus.Open();
-    DummyDevice dev(bus, 0x50);
-    dev.Init();
-    DigitalOutput led(GPIO_NUM_2, DigitalGpio::ActiveState::High);
-    led.SetActive();
+  i2c_config_t cfg{};
+  cfg.sda_io_num = GPIO_NUM_21;
+  cfg.scl_io_num = GPIO_NUM_22;
+  I2cBus bus(I2C_NUM_0, cfg);
+  bus.Open();
+  DummyDevice dev(bus, 0x50);
+  dev.Init();
+  DigitalOutput led(GPIO_NUM_2, DigitalGpio::ActiveState::High);
+  led.SetActive();
 }
